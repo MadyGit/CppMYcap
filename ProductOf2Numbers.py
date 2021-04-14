8929e61172634df553edfce600f9ec97e2298f33
Manthra H <47036388+MadyGit@users.noreply.github.com>
2021-04-14 22:18:05 +0530

    ProductOf2Numbers.py

---
+++ ProductOf2Numbers.py
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num1, num2, prod;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    prod = num1 * num2;  
+    cout << "Product = " << prod;    
+    
+    return 0;
+}
+
+//Output:Enter two numbers: 3.4
+//5.5
+//Product = 18.7
