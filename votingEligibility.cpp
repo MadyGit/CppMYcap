057b0ca0980c9c6c9899fc09499b9d77761cfbc2
Manthra H <47036388+MadyGit@users.noreply.github.com>
2021-04-21 18:10:57 +0530

    votingEligibility.cpp
    Output: 
    enter age of user:25
    
     you are eligible
    
    enter age of user:14
    
     you are not eligible

---
+++ votingEligibility.cpp
@@ -0,0 +1,17 @@
+//cpp program to check if age of user is  eligible for voting or not
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int age;
+cout<<"enter age of user:";
+ cin>>age;
+ if(age>=18)
+ {cout<<"\n you are eligible";
+  }
+  else
+  {cout<<"\n you are not eligible";
+  }
+  return 0;
+}
